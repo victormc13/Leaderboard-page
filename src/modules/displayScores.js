0cd55477c607dc016dda38156e1b36ef3b2c5f74
victormc13 <viktorsb14@gmail.com>
2023-07-26 21:31:29 -0500

    Get all scores from the API and render them on the list

---
+++ src/modules/displayScores.js
@@ -0,0 +1,12 @@
+import { getScores } from "./fetchData";
+
+export const displayScores = async () => {
+  const scores = await getScores();
+  const scoresList = document.querySelector('.score-list');
+  scoresList.innerHTML = '';
+
+  scores.forEach((score) => {
+    const scoreItem = `<li class='flex-row'>${score.user}: <span>${score.score}</span></li>`;
+    scoresList.insertAdjacentHTML('beforeend', scoreItem);
+  });
+};
