8c8132cb3222ede14a1e43cce2ee08c1ba44b972
victormc13 <viktorsb14@gmail.com>
2023-07-26 21:41:53 -0500

    Get all scores from the API and render them on the list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './style.css';
-import _ from 'lodash';
-import newGame from './modules/newGame.js';
+import { displayScores } from './modules/displayScores';
+import { postScore } from './modules/fetchData';
 
-newGame();
+const refresBtn = document.querySelector('.refresh-btn');
+refresBtn.addEventListener('click', () => {
+  displayScores();
+});
+
+const submitBtn = document.querySelector('.form');
+submitBtn.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  await postScore();
+  user.value = '';
+  score.value = '';
+  displayScores();
+});
+
+window.load = displayScores();
