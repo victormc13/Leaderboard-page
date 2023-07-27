101397146f9b55f7cfbad4651a3d19c95bc7a7fb
victormc13 <viktorsb14@gmail.com>
2023-07-26 23:45:10 -0500

    Fix linters errors

--- src/modules/newGame.js
+++ src/modules/newGame.js
@@ -12,8 +12,7 @@ const newGame = async () => {
     const response = await fetch(baseURL, requestOptions);
     const data = await response.json();
     const gameId = data.result.split(': ')[1];
-    return gameId; 
-    // console.log(gameName, 'game created with ID:', gameId);
+    return gameId; // console.log(gameName, 'game created with ID:', gameId);
   } catch (error) {
     throw new Error('Failed to create game', error);
   }
